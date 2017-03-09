00b30a5bef0b40a93d1b6abb8c8ff8820f535b6d
David <davidliang781205@gmail.com>
2017-03-09 19:37:20 +0000

    Print Waldo index in for-loop

--- findWaldo.js
+++ findWaldo.js
@@ -1,23 +1,24 @@
 // // The second argument/parameter is expected to be a function
-// function findWaldo(arr, found) {
-//   for (var i = 0; i < arr.length; i++) {
-//     if (arr[i] === "Waldo") {
-//       found();   // execute callback
-//     }
-//   }
-// }
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+      console.log(i);
+    }
+  }
+}
 
-// function actionWhenFound() {
-//   console.log("Found him!");
-// }
+function actionWhenFound() {
+  console.log("Found him!");
+}
 
-// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
+// ***************** forEach *********************
 var list = ["Alice", "Bob", "Waldo", "Winston"];
 
 list.forEach(function(para){
   if (para === "Waldo"){
-
     console.log("Found him! He is at index " + list.indexOf(para)) + '.';
   }
 })
